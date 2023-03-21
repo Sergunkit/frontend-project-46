aca49dcc962f961a8c7e750f39b05d6120974324
Sergunkit <sergunkit@mail.ru>
2023-03-21 12:48:42 +0300

    fix the bug+=6

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -3,17 +3,17 @@ const makeStylish = (diff) => {
   let stringOut = string.replace(/:/g, ': ');
   stringOut = stringOut.replace(/"/g, '');
   let deep = 0;
-  const shift = '   ';
+  const shift = '  ';
   let stylishDiff = '';
   for (let i = 0; i < stringOut.length; i += 1) {
     let j = stringOut[i];
     if (stringOut[i] === '{') {
-      deep += 1;
+      deep += 2;
       j = `{\n${shift.repeat(deep)}`;
     }
     if (stringOut[i] === '}') {
       j = `\n${shift.repeat(deep)}}`;
-      deep -= 1;
+      deep -= 2;
     }
     if (stringOut[i] === ',') {
       j = `\n${shift.repeat(deep)}`;
