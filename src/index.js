8bb79e06f48ba01dc014f433be8a3d3326cbe6d3
Serge <sergunkit@MBP-Serge.HomeLAN>
2024-07-04 21:29:20 +0300

    r

--- src/index.js
+++ src/index.js
@@ -10,3 +10,31 @@ function output(diff, opt) {
 }
 
 export default output;
+
+
+
+// import path from 'node:path';
+// import fs from 'fs';
+// import parse from './parsers.js';
+// import buildAst from './buildAST.js';
+// import formatter from './formatters/index.js';
+
+// const resolvePath = (filepath) => (filepath.includes('__fixtures__')
+//   ? path.resolve(process.cwd(), filepath)
+//   : path.resolve(process.cwd(), (`__fixtures__/${filepath}`))
+// );
+
+// const extractFormat = (filepath) => path.extname(filepath).slice(1);
+
+// const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath));
+
+// export default (filepath1, filepath2, format = 'stylish') => {
+//   const path1 = resolvePath(filepath1);
+//   const path2 = resolvePath(filepath2);
+
+//   const data1 = getData(resolvePath(path1));
+//   const data2 = getData(resolvePath(path2));
+
+//   const ast = buildAst(data1, data2);
+//   return formatter(ast, format);
+// };
--- src/parsers.js
+++ src/parsers.js
@@ -27,3 +27,13 @@ function parser(filepath) {
 }
 
 export default parser;
+
+// import yaml from 'js-yaml';
+
+// const parsers = {
+//   json: JSON.parse,
+//   yaml: yaml.load,
+//   yml: yaml.load,
+// };
+
+// export default (data, format) => parsers[format](data);
