a7e693bd071ec921981233e5b7198a63d9cff41b
Serge <sergunkit@MBP-Serge.HomeLAN>
2024-08-26 15:51:13 +0300

    back parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,28 +1,59 @@
+// import yaml from 'js-yaml';
+// import fs from 'fs';
+// import path from 'path';
+
+// function parser(filepath) {
+//   const fullPath = (fpath) => (fpath.includes('__fixtures__')
+//     ? path.resolve(process.cwd(), fpath)
+//     : path.resolve(process.cwd(), (`__fixtures__/${fpath}`))
+//   );
+
+//   if (['.yml', '.yaml'].includes(path.extname(fullPath(filepath)))) { // yaml
+//     try {
+//       return yaml.load(fs.readFileSync(fullPath(filepath), 'utf8'));
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   } else if (path.extname(fullPath(filepath)) === '.json') { // json
+//     try {
+//       return JSON.parse(fs.readFileSync(fullPath(filepath), 'utf8'));
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   } else { // txt
+//     try {
+//       return fs.readFileSync(fullPath(filepath), 'utf8');
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   }
+//   return NaN;
+// }
+
+// export default parser;
+
+
 import yaml from 'js-yaml';
 import fs from 'fs';
 import path from 'path';
 
 function parser(filepath) {
-  const fullPath = (fpath) => (fpath.includes('__fixtures__')
-    ? path.resolve(process.cwd(), fpath)
-    : path.resolve(process.cwd(), (`__fixtures__/${fpath}`))
-  );
-
-  if (['.yml', '.yaml'].includes(path.extname(fullPath(filepath)))) { // yaml
-    try {
-      return yaml.load(fs.readFileSync(fullPath(filepath), 'utf8'));
+  const fullPath = path.resolve(process.cwd(), filepath);
+  if (['.yml', '.yaml'].includes(path.extname(fullPath))) {
+    try { // yaml
+      return yaml.load(fs.readFileSync(fullPath, 'utf8'));
     } catch (err) {
       console.error(err);
     }
-  } else if (path.extname(fullPath(filepath)) === '.json') { // json
-    try {
-      return JSON.parse(fs.readFileSync(fullPath(filepath), 'utf8'));
+  } else if (path.extname(fullPath) === '.json') {
+    try { // json
+      return JSON.parse(fs.readFileSync(fullPath, 'utf8'));
     } catch (err) {
       console.error(err);
     }
-  } else { // txt
-    try {
-      return fs.readFileSync(fullPath(filepath), 'utf8');
+  } else {
+    try { // txt
+      return fs.readFileSync(fullPath, 'utf8');
     } catch (err) {
       console.error(err);
     }
