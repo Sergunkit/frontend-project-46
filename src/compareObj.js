a03c565e8c7b51bb96257346b4d99a490b777bb0
Serge <sergunkit@MBP-Serge.HomeLAN>
2024-08-29 17:03:47 +0300

    clear vers

---
+++ src/compareObj.js
@@ -0,0 +1,46 @@
+import _ from 'lodash';
+
+const compareObj = (data1, data2) => {
+  const data1Keys = _.keys(data1);
+  const data2Keys = _.keys(data2);
+  const sortedKeys = _.sortBy(_.union(data1Keys, data2Keys));
+  const nodes = sortedKeys.map((key) => {
+    if (!_.has(data1, key)) {
+      return {
+        type: 'added',
+        key,
+        value: data2[key],
+      };
+    }
+    if (!_.has(data2, key)) {
+      return {
+        type: 'removed',
+        key,
+        value: data1[key],
+      };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return {
+        type: 'nested',
+        key,
+        children: compareObj(data1[key], data2[key]),
+      };
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+      return {
+        type: 'unchanged',
+        key,
+        value: data1[key],
+      };
+    }
+    return {
+      type: 'changed',
+      key,
+      value: data1[key],
+      value2: data2[key],
+    };
+  });
+  return nodes;
+};
+
+export default compareObj;
--- src/index.js
+++ src/index.js
@@ -1,50 +1,6 @@
-import _ from 'lodash';
 import output from './formatters/index.js';
 import getObj from './parsers.js';
-
-const compareObj = (data1, data2) => {
-  const data1Keys = _.keys(data1);
-  const data2Keys = _.keys(data2);
-  const sortedKeys = _.sortBy(_.union(data1Keys, data2Keys));
-
-  const nodes = sortedKeys.map((key) => {
-    if (!_.has(data1, key)) {
-      return {
-        type: 'added',
-        key,
-        value: data2[key],
-      };
-    }
-    if (!_.has(data2, key)) {
-      return {
-        type: 'removed',
-        key,
-        value: data1[key],
-      };
-    }
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return {
-        type: 'nested',
-        key,
-        children: compareObj(data1[key], data2[key]),
-      };
-    }
-    if (_.isEqual(data1[key], data2[key])) {
-      return {
-        type: 'unchanged',
-        key,
-        value: data1[key],
-      };
-    }
-    return {
-      type: 'changed',
-      key,
-      value: data1[key],
-      value2: data2[key],
-    };
-  });
-  return nodes;
-};
+import compareObj from './compareObj.js';
 
 const gendiff = (filepath1, filepath2, option = 'stylish') => {
   const obj1 = getObj(filepath1);
